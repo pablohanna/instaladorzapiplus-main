a7f80a1d896745f2e7a471c71ca8c0e20cddb45d
ulalk system <automatizier@gmail.com>
2024-06-02 00:35:22 -0300

    update avex

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -1,54 +1,41 @@
 #!/bin/bash
 #
-# Print banner art for ChatBoard Channel.
+# Print banner art.
 
-# Definição das cores
-NC='\033[0m'            # No Color
-WHITE='\033[1;37m'     # Branco
-CYAN_LIGHT='\033[1;36m' # Ciano claro
-RED='\033[1;31m'       # Vermelho
+# Cores
+BG_BROWN='\033[48;5;94m'
+NC='\033[0m'
+WHITE='\033[1;37m'
+CYAN_LIGHT='\033[1;36m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
 
 #######################################
-# Função para imprimir o banner
+# Print a board.
 # Globals:
+#   BG_BROWN
 #   NC
 #   WHITE
 #   CYAN_LIGHT
 #   RED
+#   GREEN
+#   YELLOW
 # Arguments:
 #   None
 #######################################
 print_banner() {
-  clear  # Limpa a tela antes de imprimir o banner
+  clear
   printf "\n\n"
-  printf "${RED}##########################################################################${NC}\n"  # Linha superior do banner
-  printf "${RED}##                                                                      ##${NC}\n"
+  printf "${RED}######################################################################${NC}\n"
+  printf "${RED}##                                                                  ##${NC}\n"
   printf "${RED}##${NC}${WHITE}      _______  __    __       ___       __   __  ___   __    __      ${RED}##${NC}\n"
   printf "${RED}##${NC}${WHITE}     |       ||  |  |  |     /   \\     |  | |  ||   | |  |  |  |     ${RED}##${NC}\n"
   printf "${RED}##${NC}${WHITE}     |  .--. ||  |__|  |    /  ^  \\    |  | |  ||   | |   \\/   |     ${RED}##${NC}\n"
   printf "${RED}##${NC}${WHITE}     |  |  | ||   __   |   /  /_\  \\   |  |_|  ||   |  \\      /      ${RED}##${NC}\n"
   printf "${RED}##${NC}${WHITE}     |  '--' ||  |  |  |  /  _____  \\  |       ||   |   \\    /       ${RED}##${NC}\n"
   printf "${RED}##${NC}${WHITE}     |_______||__|  |__| /__/     \__\\ |_______||___|    \\__/        ${RED}##${NC}\n"
-  printf "${RED}##                                                                      ##${NC}\n"  # Linha inferior do banner
-  printf "${RED}##########################################################################${NC}\n\n"
-  printf "                          ${CYAN_LIGHT}© ChatBoard Channel - https://www.youtube.com/channel/UCMa9UDt137xjIEOxTwwL-Xw${NC}\n\n"
-  sleep 0.5  # Aguarda meio segundo antes de piscar
-  clear  # Limpa a tela antes de reexibir o banner
-  printf "\n\n"
-  printf "${RED}##########################################################################${NC}\n"  
-  printf "${RED}##                                                                      ##${NC}\n"
-  printf "${RED}##${NC}${WHITE}      _______  __    __       ___       __   __  ___   __    __      ${RED}##${NC}\n"
-  printf "${RED}##${NC}${WHITE}     |       ||  |  |  |     /   \\     |  | |  ||   | |  |  |  |     ${RED}##${NC}\n"
-  printf "${RED}##${NC}${WHITE}     |  .--. ||  |__|  |    /  ^  \\    |  | |  ||   | |   \\/   |     ${RED}##${NC}\n"
-  printf "${RED}##${NC}${WHITE}     |  |  | ||   __   |   /  /_\  \\   |  |_|  ||   |  \\      /      ${RED}##${NC}\n"
-  printf "${RED}##${NC}${WHITE}     |  '--' ||  |  |  |  /  _____  \\  |       ||   |   \\    /       ${RED}##${NC}\n"
-  printf "${RED}##${NC}${WHITE}     |_______||__|  |__| /__/     \__\\ |_______||___|    \\__/        ${RED}##${NC}\n"
-  printf "${RED}##                                                                      ##${NC}\n"  
-  printf "${RED}##########################################################################${NC}\n\n"
-  printf "                          ${CYAN_LIGHT}© ChatBoard Channel - https://www.youtube.com/channel/UCMa9UDt137xjIEOxTwwL-Xw${NC}\n\n"
-  sleep 0.5  # Aguarda meio segundo antes de piscar novamente
+  printf "${RED}##                                                                  ##${NC}\n"
+  printf "${RED}######################################################################${NC}\n"
+  printf "\n${CYAN_LIGHT} © CANAL ChatBoard Channel - https://www.youtube.com/channel/UCMa9UDt137xjIEOxTwwL-Xw${NC}\n\n"
 }
-
-# Chamar a função para imprimir o banner
-while true; do
-  p
